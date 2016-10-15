230252543c44aa001f48b81430dc803914b99cd8
billweasley <billweasley20092@hotmail.com>
2016-10-15 18:50:20 +0100

    initial commit

---
+++ src/emailclient/HttpInteract.java
@@ -0,0 +1,156 @@
+package emailclient;
+
+/**
+ * ***********************************
+ * Filename: HttpInteract.java Names: Student-IDs: Date:
+ * ***********************************
+ */
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Class for downloading one object from http server.
+ *
+ */
+public class HttpInteract {
+
+    private String host;
+    private String path;
+    private String requestMessage;
+
+    private static final int HTTP_PORT = 80;
+    private static final String CRLF = "\r\n";
+    private static final int BUF_SIZE = 4096;
+    private static final int MAX_OBJECT_SIZE = 102400;
+
+    /* Create a HttpInteract object. */
+    public HttpInteract(String url) {
+
+        /* Split the "URL" into "host name" and "path name", and
+		 * set host and path class variables. 
+		 * if URL is only a host name, use "/" as path 
+         */
+        int index;
+        if ((index = url.indexOf('/')) == -1) {
+            host = url;
+            path = "/";
+        } else {
+            host = url.substring(0, index);
+            path = url.substring(host.length());
+        }
+        /* Construct requestMessage, add a header line so that
+         * server closes connection after one response. 
+         */
+        requestMessage = "GET " + path + " HTTP/1.1" + CRLF + "Host: " + host + CRLF + CRLF;
+    }
+
+    /* Send Http request, parse response and return requested object 
+	 * as a String (if no errors), 
+	 * otherwise return meaningful error message. 
+	 * Don't catch Exceptions. EmailClient will handle them. */
+    public String send() throws IOException {
+
+        /* buffer to read object in 4kB chunks */
+        char[] buf = new char[BUF_SIZE];
+
+        /* Maximum size of object is 100kB, which should be enough for most objects. 
+		 * Change constant if you need more. */
+        char[] body = new char[MAX_OBJECT_SIZE];
+
+        String statusLine = "";	// status line
+        int status;		// status code
+        String headers = "";	// headers
+        int bodyLength = -1;	// lenghth of body
+
+        String[] tmp;
+
+        /* The socket to the server */
+        Socket connection;
+
+        /* Streams for reading from and writing to socket */
+        BufferedReader fromServer;
+        DataOutputStream toServer;
+
+        System.out.println("Connecting server: " + host + CRLF);
+
+        /* Connect to http server on port 80.
+        * Assign input and output streams to connection. */
+        connection = new Socket(host, HTTP_PORT);
+        fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        toServer = new DataOutputStream(connection.getOutputStream());
+
+        System.out.println("Send request:\n" + requestMessage);
+
+        /* Send requestMessage to http server */
+        toServer.writeBytes(requestMessage);
+        /* Read the status line from response message */
+        statusLine = fromServer.readLine();
+        System.out.println("Status Line:\n" + statusLine + CRLF);
+
+        /* Extract status code from status line. If status code is not 200,
+		 * close connection and return an error message. 
+		 * Do NOT throw an exception */
+        String[] temp = statusLine.split(" ");
+        if ((status = Integer.parseInt(temp[1])) != 200) {
+            return ("Error: a failure happend when accruing the file");
+        }
+
+        /* Read header lines from response message, convert to a string, 
+ 		 * and assign to "headers" variable. 
+		 * Recall that an empty line indicates end of headers.
+		 * Extract length  from "Content-Length:" (or "Content-length:") 
+		 * header line, if present, and assign to "bodyLength" variable. 
+         */
+        String headertemp;
+        while (!(headertemp = fromServer.readLine()).equals("")) {
+            headers += (headertemp + CRLF);
+            if (headertemp.contains("Content-Length:") || headertemp.contains("Content-length:")) {
+                tmp = headertemp.split(":");
+                bodyLength = Integer.parseInt(tmp[1].trim());
+            }
+        }	// requires about 10 lines of code
+        headers = headers.substring(0, (headers.length() - 4));
+        System.out.println("Headers:\n" + headers + CRLF);
+
+
+        /* If object is larger than MAX_OBJECT_SIZE, close the connection and 
+		 * return meaningful message. */
+        if (bodyLength > MAX_OBJECT_SIZE) {
+            connection.close();
+            return ("Sorry! The size of this object has a size of " + bodyLength + " bits, which is too large to get.");
+        }
+
+        /* Read the body in chunks of BUF_SIZE using buf[] and copy the chunk
+		 * into body[]. Stop when either we have
+		 * read Content-Length bytes or when the connection is
+		 * closed (when there is no Content-Length in the response). 
+		 * Use one of the read() methods of BufferedReader here, NOT readLine().
+		 * Also make sure not to read more than MAX_OBJECT_SIZE characters.
+         */
+        int bytesRead = 0;
+        int remainBytes = bodyLength;
+        if (bodyLength == -1) {
+            return ("No body length info fonded");
+        }
+        while (bytesRead < Math.min(bodyLength, MAX_OBJECT_SIZE)) {
+            try {
+                fromServer.read(buf, 0, Math.min(remainBytes, BUF_SIZE));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.arraycopy(buf, 0, body, bytesRead, Math.min(remainBytes, BUF_SIZE));
+            bytesRead += BUF_SIZE;
+            remainBytes -= BUF_SIZE;
+        }
+        // Requires 10-20 lines of code
+
+        /* At this points body[] should hold to body of the downloaded object and 
+		 * bytesRead should hold the number of bytes read from the BufferedReader
+         */
+        /* Close connection and return object as String. */
+        System.out.println("Done reading file. Closing connection.");
+        connection.close();
+        return (new String(body, 0, bytesRead));
+    }
+}
---
+++ src/emailclient/SMTPConnect.java
@@ -0,0 +1,115 @@
+package emailclient;
+
+/**
+ * ***********************************
+ * Filename: 
+ * SMTPConnect.java 
+ * Names: 
+ * Student-IDs: 
+ * Date:
+ * ***********************************
+ */
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+/**
+ * Open an SMTP connection to mailserver and send one mail.
+ *
+ */
+public class SMTPConnect {
+
+    /* The socket to the server */
+    private Socket connection;
+
+    /* Streams for reading from and writing to socket */
+    private BufferedReader fromServer;
+    private DataOutputStream toServer;
+
+    private static final int SMTP_PORT = 25;
+    private static final String CRLF = "\r\n";
+
+    /* Are we connected? Used in close() to determine what to do. */
+    private boolean isConnected = false;
+
+    /* Create an SMTPConnect object. Create the socket and the 
+       associated streams. Initialise SMTP connection. */
+    public SMTPConnect(EmailMessage mailmessage) throws IOException {
+        connection = new Socket(mailmessage.getDestHost(), SMTP_PORT);
+        fromServer = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        toServer = new DataOutputStream(connection.getOutputStream());
+        /* Read a line from server and check that the reply code is 220.
+	   If not, throw an IOException. */
+        if (!fromServer.readLine().startsWith("220")) {
+            throw new IOException("220 reply not received from server.");
+        }
+
+        /* SMTP handshake. We need the name of the local machine.
+	   Send the appropriate SMTP handshake command. */
+        String localhost = InetAddress.getLocalHost().getHostName();
+        try {
+            sendCommand("HELO " + localhost + CRLF, 250);
+            isConnected = true;
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("SMTP Handshaking error.");
+        }
+
+    }
+
+    /* Send the message. Write the correct SMTP-commands in the
+       correct order. No checking for errors, just throw them to the
+       caller. */
+    public void send(EmailMessage mailmessage) throws IOException {
+
+        String cammand;
+        /* Send all the necessary commands to send a message. Call
+	   sendCommand() to do the dirty work. Do _not_ catch the
+	   exception thrown from sendCommand(). */
+        cammand = "MAIL FROM: <" + mailmessage.getSender() + ">" + CRLF;
+        sendCommand(cammand, 250);
+        cammand = "RCPT TO: <" + mailmessage.getRecipient() + ">" + CRLF;
+        sendCommand(cammand, 250);
+        cammand = "DATA" + CRLF;
+        sendCommand(cammand, 354);
+        cammand = mailmessage.getHeaders() + CRLF + mailmessage.getBody() + CRLF + "." + CRLF;
+        toServer.writeBytes(cammand);
+
+    }
+
+    /* Close SMTP connection. First, terminate on SMTP level, then
+       close the socket. */
+    public void close() {
+        isConnected = false;
+        try {
+            sendCommand("QUIT" + CRLF, 250);  //?221
+            connection.close();
+        } catch (IOException e) {
+            System.out.println("Unable to close connection: " + e);
+            e.printStackTrace();
+            isConnected = true;
+        }
+    }
+
+    /* Send an SMTP command to the server. Check that the reply code is
+       what is is supposed to be according to RFC 821. */
+    private void sendCommand(String command, int rc) throws IOException {
+        toServer.writeBytes(command);
+        /* Write command to server and read reply from server. */
+        String msg = fromServer.readLine();
+        /* Check that the server's reply code is the same as the parameter
+	   rc. If not, throw an IOException. */
+        if (!msg.startsWith(String.valueOf(rc))) {
+            throw new IOException(command + " executed error:" + rc + " reply not received from server.");
+        }
+    }
+
+    /* Destructor. Closes the connection if something bad happens. */
+    @Override
+    protected void finalize() throws Throwable {
+        if (isConnected) {
+            close();
+        }
+        super.finalize();
+    }
+}
